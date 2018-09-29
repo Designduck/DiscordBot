e6a8a1d52dedbb98f95cc957d3252bcddb4f7eb4
Andreas Munter <anm@klasselotteriet.dk>
2018-09-29 16:47:34 +0200

    Time test, and so it begins

--- bot.js
+++ bot.js
@@ -18,22 +18,31 @@ bot.on('ready', function (evt) {
     logger.info(bot.username + ' - (' + bot.id + ')');
 });
 bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
+    // Will listen for messages that will start with `!`
     if (message.substring(0, 1) == '!') {
         var args = message.substring(1).split(' ');
         var cmd = args[0];
        
         args = args.splice(1);
         switch(cmd) {
-            // !ping
-            case 'ping':
+            // HELP command
+            case 'help':
                 bot.sendMessage({
                     to: channelID,
-                    message: 'Pong!'
+                    message: ':ambulance: Help will come soon.'
                 });
             break;
-            // Just add any case commands if you want to..
+            // Test of time
+            case 'time':
+                var d = new Date();
+                var h = (d.getHours()<10?'0':'') + d.getHours();
+                var m = (d.getMinutes()<10?'0':'') + d.getMinutes();
+                var now = h + ':' + m;
+                bot.sendMessage({
+                    to: channelID,
+                    message: now
+                });
+            break;
          }
      }
 });
